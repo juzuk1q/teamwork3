443c94cf10552398fce89d233ceaf4d65f6c3fe4
juzuk1q <artemtselikov2011@gmail.com>
2025-12-18 11:48:37 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.madeby = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Антон";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+            this.label1.Location = new System.Drawing.Point(202, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Практическая работа №3";
+            // 
+            // madeby
+            // 
+            this.madeby.Location = new System.Drawing.Point(560, 392);
+            this.madeby.Name = "madeby";
+            this.madeby.Size = new System.Drawing.Size(228, 49);
+            this.madeby.TabIndex = 4;
+            this.madeby.Text = "Сделали студенты 419 группы: Хаки Антон, Целиков Артём, Шаманиди Сергей";
+            this.madeby.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сергей";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 71);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Артём";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.madeby);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "helloWorld";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label madeby;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
