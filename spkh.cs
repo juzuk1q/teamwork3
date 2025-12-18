eceed08a4a1131833dab73e8808ad1bb4d1e976e
419 <419@first.local>
2025-12-18 14:54:33 +0500

    Добавлен кликер.

--- spkh.cs
+++ spkh.cs
@@ -12,9 +12,21 @@ namespace project1
 {
     public partial class spkh : Form
     {
+        int counter = 0;
         public spkh()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = counter.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
