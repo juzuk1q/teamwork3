a029a2ddd9a9783697de8040cb5a0907455c7b29
juzuk1q <artemtselikov2011@gmail.com>
2025-12-18 14:31:54 +0300

    Добавлена игра на экране Артёма

--- juzuk1q.Designer.cs
+++ juzuk1q.Designer.cs
@@ -28,12 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.newGame_btn = new System.Windows.Forms.Button();
+            this.lblMove = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.winX = new System.Windows.Forms.Label();
+            this.winO = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(404, 362);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(16, 15);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(108, 98);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(145, 15);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(108, 98);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(274, 15);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(108, 98);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(274, 131);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(108, 98);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(145, 131);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(108, 98);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(16, 131);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(108, 98);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(274, 246);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(108, 98);
+            this.btn9.TabIndex = 5;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(145, 246);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(108, 98);
+            this.btn8.TabIndex = 4;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(16, 246);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(108, 98);
+            this.btn7.TabIndex = 3;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // newGame_btn
+            // 
+            this.newGame_btn.Location = new System.Drawing.Point(462, 256);
+            this.newGame_btn.Name = "newGame_btn";
+            this.newGame_btn.Size = new System.Drawing.Size(144, 68);
+            this.newGame_btn.TabIndex = 1;
+            this.newGame_btn.Text = "Новая игра";
+            this.newGame_btn.UseVisualStyleBackColor = true;
+            this.newGame_btn.Click += new System.EventHandler(this.newGame_btn_Click);
+            // 
+            // lblMove
+            // 
+            this.lblMove.AutoSize = true;
+            this.lblMove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F);
+            this.lblMove.Location = new System.Drawing.Point(437, 19);
+            this.lblMove.Name = "lblMove";
+            this.lblMove.Size = new System.Drawing.Size(186, 29);
+            this.lblMove.TabIndex = 2;
+            this.lblMove.Text = "Текущий ход:  ";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(440, 55);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 1);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(536, 55);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1, 45);
+            this.panel3.TabIndex = 4;
+            // 
+            // winX
+            // 
+            this.winX.AutoSize = true;
+            this.winX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.winX.Location = new System.Drawing.Point(437, 71);
+            this.winX.Name = "winX";
+            this.winX.Size = new System.Drawing.Size(87, 20);
+            this.winX.TabIndex = 5;
+            this.winX.Text = "Счёт Х: 0";
+            // 
+            // winO
+            // 
+            this.winO.AutoSize = true;
+            this.winO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.winO.Location = new System.Drawing.Point(547, 71);
+            this.winO.Name = "winO";
+            this.winO.Size = new System.Drawing.Size(88, 20);
+            this.winO.TabIndex = 6;
+            this.winO.Text = "Счёт O: 0";
+            // 
+            // juzuk1q
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(657, 392);
+            this.Controls.Add(this.winO);
+            this.Controls.Add(this.winX);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.lblMove);
+            this.Controls.Add(this.newGame_btn);
+            this.Controls.Add(this.panel1);
+            this.Name = "juzuk1q";
             this.Text = "juzuk1q";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button newGame_btn;
+        private System.Windows.Forms.Label lblMove;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label winX;
+        private System.Windows.Forms.Label winO;
     }
 }
--- juzuk1q.cs
+++ juzuk1q.cs
@@ -12,9 +12,120 @@ namespace project1
 {
     public partial class juzuk1q : Form
     {
+        bool xTurn = true;
+        int moves = 0;
+        int xScore = 0;
+        int oScore = 0;
+
         public juzuk1q()
         {
             InitializeComponent();
+            UpdateTurnLabel();
         }
+
+        private void UpdateTurnLabel()
+        {
+            lblMove.Text = $"Текущий ход: {(xTurn ? "X" : "O")}";
+            lblMove.ForeColor = xTurn ? Color.Blue : Color.Red;
+        }
+
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Text != "")
+                return;
+
+            btn.Text = xTurn ? "X" : "O";
+            btn.ForeColor = xTurn ? Color.Blue : Color.Red;
+            btn.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
+
+            moves++;
+
+            if (CheckWin())
+            {
+                if (xTurn)
+                {
+                    xScore++;
+                    winX.Text = $"Счёт X: {xScore}";
+                }
+                else
+                {
+                    oScore++;
+                    winO.Text = $"Счёт О: {oScore}";
+                }
+
+                MessageBox.Show($"Победил {(xTurn ? "X" : "O")}!");
+                DisableField();
+                return;
+            }
+
+            if (moves == 9)
+            {
+                MessageBox.Show("Ничья!");
+                return;
+            }
+
+            xTurn = !xTurn;
+            UpdateTurnLabel();
+        }
+
+        private bool CheckWin()
+        {
+            Button[,] b =
+            {
+            { btn1, btn2, btn3 },
+            { btn4, btn5, btn6 },
+            { btn7, btn8, btn9 }
+        };
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (b[i, 0].Text != "" && b[i, 0].Text == b[i, 1].Text && b[i, 1].Text == b[i, 2].Text)
+                    return true;
+
+                if (b[0, i].Text != "" && b[0, i].Text == b[1, i].Text && b[1, i].Text == b[2, i].Text)
+                    return true;
+            }
+
+            if (b[0, 0].Text != "" && b[0, 0].Text == b[1, 1].Text && b[1, 1].Text == b[2, 2].Text)
+                return true;
+
+            if (b[0, 2].Text != "" && b[0, 2].Text == b[1, 1].Text && b[1, 1].Text == b[2, 0].Text)
+                return true;
+
+            return false;
+        }
+        private void DisableField()
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is Button && c.Name.StartsWith("btn"))
+                    c.Enabled = false;
+            }
+        }
+
+        private void newGame_btn_Click(object sender, EventArgs e)
+        {
+            Button[] buttons =
+            {
+        btn1, btn2, btn3,
+        btn4, btn5, btn6,
+        btn7, btn8, btn9
+    };
+
+            foreach (Button btn in buttons)
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+
+            xTurn = true;
+            moves = 0;
+            UpdateTurnLabel();
+        }
+
+
     }
 }
